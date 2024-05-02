a6154ade13f7774422b5618367dbb939796e9c42
MyBFMoti <jiny5388@gmail.com>
2024-05-02 15:24:08 +0900

    회사 소개 페이지 Introduction.js 추가

--- web-shop/proto/src/App.js
+++ web-shop/proto/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import IntroductionPage from './pages/introduction/IntroductionPage';
 
 
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path='/introduction' element={<IntroductionPage />} />
         </Routes>
       </Router>
   );
---
+++ web-shop/proto/src/pages/introduction/IntroductionPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//components
+
+import NavBar from '../../components/NavBar';
+
+import Footer from '../../components/Footer';
+
+
+
+
+
+function IntroductionPage() {
+  return (
+    <div>
+      <NavBar />
+      
+      <Footer />
+    </div>
+  );
+}
+
+
+export default IntroductionPage;
