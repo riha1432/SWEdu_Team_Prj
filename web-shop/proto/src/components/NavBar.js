064395cebfc67c293d0ec14e72d73146c0cffd3c
MyBFMoti <jiny5388@gmail.com>
2024-05-03 13:35:38 +0900

    NavBaer.js 상단 고정으로 변경

--- web-shop/proto/src/components/NavBar.js
+++ web-shop/proto/src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar2 = () => {
   return (
     <navBar2 className='App'>
-      <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <nav className="navbar fixed-top navbar-expand-lg bg-dark" data-bs-theme="dark">
           <div className="container-fluid">
               <a className="navbar-brand" href="/">Ai농가집성마켓</a>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
