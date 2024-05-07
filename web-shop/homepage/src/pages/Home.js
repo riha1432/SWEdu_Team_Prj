5568b7c1e68ff61264aac3d2f28c9740e3c9c79d
MyBFMoti <jiny5388@gmail.com>
2024-05-07 16:56:37 +0900

    home.js 홈페이지 이달의 상품 목록 레이아웃 크기 조절

--- web-shop/homepage/src/pages/Home.js
+++ web-shop/homepage/src/pages/Home.js
@@ -1,26 +1,25 @@
 import React from 'react';
 
-//components
-
+// 컴포넌트 임포트
 import NavBar from '../components/NavBar';
 import Carousel from '../components/Carousels';
 import TodayList from '../components/TodayList';
 import Footer from '../components/Footer';
 
-
-
-
-
+// Home 컴포넌트
 function Home() {
   return (
     <div className="App">
       <NavBar />
       <Carousel />
-      <TodayList />
+      <div style={{ display: 'flex', justifyContent: 'left', width: '100%' }}>
+        <div style={{ flexBasis: '50%' }}> {/* 가로 길이의 절반만 사용 */}
+          <TodayList />
+        </div>
+      </div>
       <Footer />
     </div>
   );
 }
-
 
 export default Home;
