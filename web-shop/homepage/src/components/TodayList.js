e35217ae6b023d00ed59ee6355f8ff425f9a50ee
MyBFMoti <jiny5388@gmail.com>
2024-05-07 15:48:07 +0900

    이미지 파일을 불러오는 경로 수정

--- web-shop/homepage/src/components/TodayList.js
+++ web-shop/homepage/src/components/TodayList.js
@@ -1,31 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import kaleImage from '../images/kaleImage.jpg'
+
+// 채소 데이터 배열
+const vegetables = [
+  { name: 'Carrots', src: 'https://via.placeholder.com/150', path: '/vegetable/carrots' },
+  { name: 'Broccoli', src: 'https://via.placeholder.com/150', path: '/vegetable/broccoli' },
+  { name: 'Kale', src: kaleImage, path: '/vegetable/kale' },
+  // 새로운 채소 추가
+];
+
+// 컴포넌트 스타일
+const galleryStyle = {
+  display: 'flex',
+  gap: '10px',
+  justifyContent: 'flex-start',
+};
+
+const imgStyle = {
+  width: '150px',
+  height: '150px',
+  objectFit: 'cover',
+  borderRadius: '50%',
+};
+
 function VegetableGallery() {
   return (
-    <div style={{ display: 'flex', gap: '10px', justifyContent: 'left' }}>
-      {/* 채소 이미지와 링크 */}
-      <Link to="/vegetable/carrots">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="Carrots"
-          style={{ width: '150px', height: '150px', objectFit: 'cover' }}
-        />
-        <p style={{ textAlign: 'center' }}>Carrots</p>
-      </Link>
-
-      <Link to="/vegetable/broccoli">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="Broccoli"
-          style={{ width: '150px', height: '150px', objectFit: 'cover' }}
-        />
-        <p style={{ textAlign: 'center' }}>Broccoli</p>
-      </Link>
-
-      {/* 다른 채소 추가 */}
+    <div style={galleryStyle}>
+      {vegetables.map((veg, index) => (
+        <Link key={index} to={veg.path}>
+          <img
+            src={veg.src}
+            alt={veg.name}
+            style={imgStyle}
+          />
+          <p style={{ textAlign: 'center' }}>{veg.name}</p>
+        </Link>
+      ))}
     </div>
   );
 }
 
 export default VegetableGallery;
+
