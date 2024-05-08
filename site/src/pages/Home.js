25b0f8d247917b0e4f48a8679fcfe82c53652371
MyBFMoti <jiny5388@gmail.com>
2024-05-08 16:48:43 +0900

    Home.js : 이달의 채소 리스트 왼쪽으로 배치, 그리고 오른쪽에 임시 게시판 표시

--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,26 +1,48 @@
 import React from 'react';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 
-//components
-
+// Components
 import NavBar from '../components/NavBar';
 import Carousel from '../components/Carousels';
 import TodayList from '../components/TodayList';
 import Footer from '../components/Footer';
 
-
-
-
+function BulletinBoard() {
+  return (
+    <div>
+      <h1>게시판 제목</h1>
+      <Card>
+      <Card.Header>문의 게시판</Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item>게시글 1</ListGroup.Item>
+        <ListGroup.Item>게시글 2</ListGroup.Item>
+        <ListGroup.Item>게시글 3</ListGroup.Item>
+      </ListGroup>
+      </Card>
+    </div>
+    
+  );
+}
 
 function Home() {
   return (
     <div className="App">
       <NavBar />
       <Carousel />
-      <TodayList />
+      {/* Container를 사용하여 레이아웃 감싸기 */}
+      <Container fluid>
+        <Row > {/* 한 줄에 두 개의 컬럼 */}
+          <Col xs={12} md={6}> {/* 작은 화면에서는 전체 너비, 큰 화면에서는 반씩 */}
+            <TodayList />
+          </Col>
+          <Col xs={12} md={6}>  
+            <BulletinBoard />
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
 }
-
 
 export default Home;
