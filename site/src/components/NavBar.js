a0efdcf644e7987b31ae84d5d23c4f49461e4ffe
MegatoneMan <asyy79571@gmail.com>
2024-05-10 16:22:26 +0900

    navbar:로그인 기록 남기고 로그아웃하면 로그인 기록 사라짐,로그인하면 마이 페이지보이고 로그인 회원가입 사라지고 로그아웃하면 그 반대

--- site/src/components/NavBar.js
+++ site/src/components/NavBar.js
@@ -1,44 +1,55 @@
-/*eslint-disable*/
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const isLoggedIn = localStorage.getItem('token') !== null;
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // 토큰 제거
+    // 로그아웃 후 추가적인 작업 수행 (예: 리디렉션 등)
+  };
+
   return (
-    <navBar className='App'>
-      <nav className="navbar fixed-top navbar-expand-lg bg-dark" data-bs-theme="dark">
-          <div className="container-fluid">
-              <a className="navbar-brand" href="/">Ai농가집성마켓</a>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarColor01">
-                <ul className="navbar-nav me-auto">
-                  <li className="nav-item">
-                    <a className="nav-link" href="/introduction">회사 소개</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link " href="/login">로그인
-                      <span className="visually-hidden">(current)</span>
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="/signup">회원가입</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="/Product">상품목록</a>
-                  </li>
-                  
-                  
-                </ul>
-                <form className="d-flex">
-                  <input class="form-control mr-sm-2" style={{width: "250px"}} type="search" placeholder="Search" aria-label="Search"/>
-                  <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                </form>
-              </div>
+    <nav className="navbar fixed-top navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Ai농가집성마켓</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarColor01">
+          <ul className="navbar-nav me-auto">
+            {!isLoggedIn && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">로그인</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">회원가입</Link>
+                </li>
+              </>
+            )}
+            <li className="nav-item">
+              <Link className="nav-link" to="/introduction">회사 소개</Link>
+            </li>
+          </ul>
+          {isLoggedIn && (
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="/" role="button" aria-haspopup="true" aria-expanded="false">마이페이지</a>
+                <div className="dropdown-menu">
+                  <Link className="dropdown-item" to="/">회원 정보</Link>
+                  <Link className="dropdown-item" to="/">배송 조회</Link>
+                  <Link className="dropdown-item" to="/">수량 변경</Link>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="/" onClick={handleLogout}>로그아웃</a>
+                </div>
+              </li>
+            </ul>
+          )}
         </div>
-      </nav>
-    </navBar>
+      </div>
+    </nav>
   );
 };
 
 export default NavBar;
-
