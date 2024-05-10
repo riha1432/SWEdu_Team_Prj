7e2af5ef542aad3a93e99e0f0546b3546d22dbbf
MegatoneMan <asyy79571@gmail.com>
2024-05-10 16:29:16 +0900

    app.js 건든거

--- site/src/App.js
+++ site/src/App.js
@@ -1,34 +1,36 @@
-/*eslint-disable*/
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer'
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
-import Product from './pages/product/Product';
-import IntroductionPage from './pages/introduction/IntroductionPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import NavBar from './components/NavBar';
 
 function App() {
+  // 로그인 상태를 저장하는 state 변수
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 로그인 상태를 변경하는 함수
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  // 로그아웃 상태를 변경하는 함수
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-  <div>
-    <NavBar />
     <Router>
-        <Routes >
+      <div>
+        <NavBar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path='/introduction' element={<IntroductionPage />} />
-          <Route path='/product' element={<Product />} />
         </Routes>
-      </Router>
-      <Footer />
-  </div>
-      
+      </div>
+    </Router>
   );
 }
 
