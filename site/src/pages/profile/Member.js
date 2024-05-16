b67892fcdaea9fbfd46fe47a63fb10b4f185ab94
MegatoneMan <asyy79571@gmail.com>
2024-05-16 17:03:53 +0900

    member: 디자인 1단계

--- site/src/pages/profile/Member.js
+++ site/src/pages/profile/Member.js
@@ -23,7 +23,6 @@ const Member = () => {
       }
     };
 
-    
     fetchUserData();
   }, []);
 
@@ -67,58 +66,79 @@ const Member = () => {
   return (
     <>
       <div className='box-subTop ani-on'>
-            <div className="bg">
-                <div class="hide-on-pc" style={{ backgrounImage: 'url(${backgroundImageUrl}()' }} />
-                <div class="hide-on-mobile" style={{ backgrounImage: 'url(${backgroundImageUrl}()' }} />
-            </div>
-            <div className="txt-wrap">
-                <div className="txt-inner">
-                    <h2 className="hidden">조회/예약</h2>
-                    <h3 className="title-1 ani-1 delay-1 member-title">회원 정보 및 수정하기</h3>
-                    <p className="txt-1 ani-1 delay-2">Ai농가집성마켓은 고객님의 소중한 마음까지 전달합니다.</p>
-                </div>
-            </div>
+        <div className="bg">
+          <div class="hide-on-pc" style={{ backgroundImage: 'url(${backgroundImageUrl}()' }} />
+          <div class="hide-on-mobile" style={{ backgroundImage: 'url(${backgroundImageUrl}()' }} />
         </div>
-      <div className="member-container">
-      {userData ? (
-        <div>
-          <div className="member-info">
-            <p>이름: {userData.Signup_name}</p>
-            <p>이메일: {userData.Signup_email}</p>
-            <p>비밀번호: {userData.Signup_pw}</p>
-            <p>주소: {userData.Signup_addr}</p>
-            <p>전화번호: {userData.Signup_ph}</p>
+        <div className="txt-wrap">
+          <div className="txt-inner">
+            <h2 className="hidden">조회/예약</h2>
+            <h3 className="title-1 ani-1 delay-1 member-title">회원 정보 및 수정하기</h3>
+            <p className="txt-1 ani-1 delay-2">Ai농가집성마켓은 고객님의 소중한 마음까지 전달합니다.</p>
           </div>
-          {isEditing ? (
-            <div className="edit-container">
-              <div className="input-container">
-                <label>이름:</label>
-                <input type="text" name="Signup_name" value={editedUserData.Signup_name} onChange={handleChange} />
-              </div>
-              <div className="input-container">
-                <label>비밀번호:</label>
-                <input type="text" name="Signup_pw" value={editedUserData.Signup_pw} onChange={handleChange} />
-              </div>
-              <div className="input-container">
-                <label>주소:</label>
-                <input type="text" name="Signup_addr" value={editedUserData.Signup_addr} onChange={handleChange} />
-              </div>
-              <div className="input-container">
-                <label>전화번호:</label>
-                <input type="text" name="Signup_ph" value={editedUserData.Signup_ph} onChange={handleChange} />
-              </div>
-              <button className="save-button" onClick={handleSave}>저장</button>
-            </div>
-          ) : (
-            <button className="edit-button" onClick={handleEdit}>수정하기</button>
-          )}
-
         </div>
-      ) : (
-        <p>회원 정보를 불러오는 중...</p>
-      )}
-    </div>
-</>
+      </div>
+      <div className="member-container">
+        {userData ? (
+          <div className="member-info">
+            {isEditing ? (
+              <>
+                <div className="input-container">
+                  <label>이름:</label>
+                  <input type="text" name="Signup_name" value={editedUserData.Signup_name} onChange={handleChange} />
+                </div>
+                <div className="input-container">
+                  <label>비밀번호:</label>
+                  <input type="text" name="Signup_pw" value={editedUserData.Signup_pw} onChange={handleChange} />
+                </div>
+                <div className="input-container">
+                  <label>주소:</label>
+                  <input type="text" name="Signup_addr" value={editedUserData.Signup_addr} onChange={handleChange} />
+                </div>
+                <div className="input-container">
+                  <label>전화번호:</label>
+                  <input type="text" name="Signup_ph" value={editedUserData.Signup_ph} onChange={handleChange} />
+                </div>
+                <button onClick={handleSave}>저장하기</button> {/* 저장하기 버튼 추가 */}
+              </>
+            ) : (
+              <>
+                <div className="middle">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <th>이름</th>
+                        <td>{userData.Signup_name}</td>
+                      </tr>
+                      <tr>
+                        <th>이메일</th>
+                        <td>{userData.Signup_email}</td>
+                      </tr>
+                      <tr>
+                        <th>비밀번호</th>
+                        <td>{userData.Signup_pw}</td>
+                      </tr>
+                      <tr>
+                        <th>주소</th>
+                        <td>{userData.Signup_addr}</td>
+                      </tr>
+                      <tr>
+                        <th>전화번호</th>
+                        <td>{userData.Signup_ph}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <button onClick={handleEdit}>수정하기</button> {/* 수정하기 버튼 */}
+              </>
+            )}
+          </div>
+        ) : (
+          <p>회원 정보를 불러오는 중...</p>
+        )}
+      </div>
+    </>
   );
 };
+
 export default Member;
